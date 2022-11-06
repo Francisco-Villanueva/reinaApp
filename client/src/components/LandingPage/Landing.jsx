5e062236bf1f7d889e4186e2c0a366a52c33ae74
unknown <franciscovillanuevaj99@gmail.com>
2022-11-05 23:56:42 -0300

    lp commit2

--- client/src/components/LandingPage/Landing.jsx
+++ client/src/components/LandingPage/Landing.jsx
@@ -3,23 +3,27 @@ import './Landing.css'
 import logo from '../../images/logo.jpg'
 import fondo from '../../images/fondoImg.jpg'
 import {Link} from 'react-router-dom'
+import{FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import{faHeadset, faBurger } from '@fortawesome/free-solid-svg-icons'
 export default function Landing() {
   return (
     <div className='landing-mainContainer'>
-      <div className='img-container'>
+      <div className='img-container-landing'>
         <img src={logo}/>
       </div>
 
         <div className='cards-Container'>
           <Link  className='link-style' to={'/home'}>
             <div className='cardLanding'>
-             <p>REINA BURGUESA</p>
+            <FontAwesomeIcon className='icon-cardLanding' icon={faHeadset}/>
+             <p>ATENCION AL CLIENTE</p>
             <span> Tomar pedidos </span>
             </div>
           </Link>
           <Link  className='link-style' to={'/menu'}>
            <div className='cardLanding'>
-            <p>MENU</p>
+           <FontAwesomeIcon className='icon-cardLanding' icon={faBurger}/>
+            <p>MENÚ</p>
             <span>Editar y ver menú</span>
             </div>
           </Link>
--- client/src/components/Menu/burgerCard/CardB.jsx
+++ client/src/components/Menu/burgerCard/CardB.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import './CardB.css'
 import {Link} from 'react-router-dom'
+import {FontAwesomeIcon }from '@fortawesome/react-fontawesome'
+import {faPenToSquare }from '@fortawesome/free-solid-svg-icons'
 export default function CardB(props) {
   console.log(props)
   return (
     <div className='card-main'>
       <div className='data-container'>
-        <h3>{props.name}</h3>
-        <p>{props.description}</p>
+        <div className='items'>
+
+        Nombre: 
+        <b>{props.name}</b>
+        </div>
+        <div className='items'>
+
+        Descriocion:
+        <b> {props.description}</b>
+        </div>
+        <div className='items'>
+
+        Precio: 
         <b>$ {props.price}</b>
-      </div>
-      <div className='btns-container'>
-        <Link to={`/editburger/${props.id}`}>edit</Link>
-      </div>
+        </div>
+       </div>
+        <div className='btns-container'>
+          <Link className='btn-edit' to={`/editburger/${props.id}`}><FontAwesomeIcon icon={faPenToSquare} /></Link>
+        </div>
     </div> 
   )
 }
