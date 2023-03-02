00077baedce9c80dde6672ad195d029acf34fe24
Francisco Villanueva <franciscovillanuevaj99@gmail.com>
2023-03-02 17:14:53 -0300

    ¿¿

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,8 @@
 const server = require("./app");
 const { conn } = require("./db");
 
-conn.sync({alter: true }).then(() => {
-  server.listen(4000, () => {
+conn.sync({ alter: true }).then(() => {
+  server.listen(process.env.PORT || 4000, () => {
     console.log("%s server runing on port 4000");
   });
 });
