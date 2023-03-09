41e32ca6571ea6435c04e4c7fe7086d48e58af6c
Francisco Villanueva <franciscovillanuevaj99@gmail.com>
2023-03-08 23:40:47 -0300

    editin styling

--- client/src/components/Menu/FormEdit/CreateForm.jsx
+++ client/src/components/Menu/FormEdit/CreateForm.jsx
@@ -4,7 +4,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { createBurger, editBurger } from "../../../actions";
 import BackBtn from "../../Btn-backNavigate/BackBtn";
 import { getBurgerInfo } from "../../../actions";
-
+import "./CreateForm.css";
 export default function CreateForms({ editing }) {
   // console.log(editing);
   const { id } = useParams();
@@ -28,41 +28,52 @@ export default function CreateForms({ editing }) {
   };
   return (
     <div>
-      <div className="form-main">
+      <div className="form-main ">
         <BackBtn />
 
-        <div className="burgerData-container">
-          <h3>Completar datos :</h3>
-          <input
-            className="inputEditBurg"
-            autoComplete="off"
-            name="name"
-            value={data.name}
-            onChange={handleInput}
-            type="text"
-            placeholder="Nombre"
-          />
-          <input
-            className="inputEditBurg"
-            autoComplete="off"
-            name="description"
-            value={data.description}
-            onChange={handleInput}
-            type="text"
-            placeholder="Descripcion"
-          />
-          <input
-            className="inputEditBurg"
-            autoComplete="off  "
-            name="price"
-            value={data.price}
-            onChange={handleInput}
-            type="number"
-            placeholder="Precio"
-          />
-          <button className="btn-editBurg" onClick={handleEdit}>
-            Cargar
-          </button>
+        <div className="burgerData-container color-change-2x scale-in-top">
+          <div>
+            <h3 className="tracking-in-expand ">COMPLETAR DATOS </h3>
+          </div>
+          <div className="body-form-cont">
+            <div className="inputs-cont">
+              <input
+                className="inputEditBurg scale-in-hor-left"
+                autoComplete="off"
+                name="name"
+                value={data.name}
+                onChange={handleInput}
+                type="text"
+                placeholder="Nombre"
+              />
+              <input
+                className="inputEditBurg scale-in-hor-left"
+                autoComplete="off"
+                name="description"
+                value={data.description}
+                onChange={handleInput}
+                type="text"
+                placeholder="Descripcion"
+              />
+              <input
+                className="inputEditBurg scale-in-hor-left"
+                autoComplete="off  "
+                name="price"
+                value={data.price}
+                onChange={handleInput}
+                type="number"
+                placeholder="Precio"
+              />
+            </div>
+            <div>
+              <button
+                className="btn-editBurg scale-in-ver-center"
+                onClick={handleEdit}
+              >
+                {editing ? "Editar" : "Cargar "}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/Menu/burgerCard/CardB.jsx
+++ client/src/components/Menu/burgerCard/CardB.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CardB.css";
 import { useDispatch } from "react-redux";
 import { deleteBurger } from "../../../actions";
@@ -6,13 +6,19 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 export default function CardB(props) {
+  const [del, setDel] = useState(false);
   console.log(props);
   const dispatch = useDispatch();
   const handleDeleteBurger = () => {
+    setDel(true);
     dispatch(deleteBurger(props.id));
   };
+  const claseCard = !del
+    ? "card-main scale-in-hor-left "
+    : "card-main scale-out-vertical";
+
   return (
-    <div className="card-main scale-in-hor-left ">
+    <div className={claseCard}>
       <div className="data-container">
         <div className="items">
           <h2 className="tracking-in-expand">{props.name} ---------- </h2>
@@ -32,7 +38,7 @@ export default function CardB(props) {
           </div>
         </div>
       </div>
-      <div className="btns-container scale-in-ver-center">
+      <div className="btns-container scale-in-ver-centerBB">
         <Link className="btn-edit" to={`/editburger/${props.id}`}>
           <FontAwesomeIcon icon={faPenToSquare} />
         </Link>
